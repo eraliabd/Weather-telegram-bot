1d277920f58d4b6d7922e68ba55d3a2ce69f9ab0
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2022-07-30 09:11:53 +0500

    commit

--- main_file.py
+++ main_file.py
@@ -49,7 +49,6 @@ def message_handler(update, context):
 
     date = datetime.datetime.now()
     now = date.strftime("%Y-%m-%d %H:%M")
-    # date_add = datetime.datetime(2022, 7, 30, 5, 00, 00)
 
     for i in range(len(viloyat)):
         button = [[InlineKeyboardButton(text="Viloyatlar 👉", callback_data="viloyat")],
